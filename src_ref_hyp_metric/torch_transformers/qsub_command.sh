8d8f217136ebe074dd0d78f7153d93789eea583b
kosuketa <deanagron@gmail.com>
2020-09-09 19:37:56 +0900

    updated

--- src_ref_hyp_metric/torch_transformers/qsub_command.sh
+++ src_ref_hyp_metric/torch_transformers/qsub_command.sh
@@ -2,7 +2,7 @@
 #$ -jc gpu-container_g4
 #$ -cwd
 #$ -ac d=nvcr-pytorch-2003
-#$ -v  PYTHONPATH="/home/ksudoh/kosuke-t/.pyenv/versions/3.6.9/envs/src_ref_hyp_metric/lib/python3.6/site-packages:$PYTHONPATH"
+### $ -v  PYTHONPATH="/home/ksudoh/kosuke-t/.pyenv/versions/3.6.9/envs/src_ref_hyp_metric/lib/python3.6/site-packages:$PYTHONPATH"
 #$ -m be
 #$ -M takahashi.kosuke.th0@is.naist.jp
 #$ -j y
